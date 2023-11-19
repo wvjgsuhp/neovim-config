8b0753a58670f9b14a990d7f1b95be399a736a23
wasin pipattungsakul <wasin.pas@hotmail.com>
2023-11-19 14:01:19 +1030

    push missing file

---
+++ lua/interface.lua
@@ -0,0 +1,7 @@
+function _G.adjust_lead_multispace()
+  local lead = "┊"
+  for _ = 1, vim.bo.shiftwidth - 1 do
+    lead = lead .. " "
+  end
+  vim.opt_local.listchars:append({ leadmultispace = lead })
+end
