88e14aedc0de878065b4abd1ee3aeafb15193310
wvjgsuhp <wasin.pas@hotmail.com>
2024-06-14 11:11:47 +0930

    add vscode

--- lua/init.lua
+++ lua/init.lua
@@ -1,7 +1,12 @@
-require("settings")
-require("lazy-bootstrap")
+if not vim.g.vscode then
+  require("settings")
+  require("lazy-bootstrap")
 
-require("lines")
-require("interface")
+  require("lines")
+  require("interface")
+else
+  require("lazy-bootstrap-vscode")
+  vim.o.spell = false
+end
 
 pcall(require, "local")
