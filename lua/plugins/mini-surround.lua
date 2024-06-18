6b0d59c0b424c6647bbca733f0a9e77ffeb69df2
wvjgsuhp <wasin.pas@hotmail.com>
2024-06-18 23:28:36 +0930

    update mappings

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -2,29 +2,35 @@ return {
   "echasnovski/mini.surround",
   event = { "BufReadPre", "BufNewFile" },
   version = "*",
-  opts = {
-    -- Add custom surroundings to be used on top of builtin ones. For more
-    -- information with examples, see `:h MiniSurround.config`.
-    custom_surroundings = nil,
+  config = function()
+    local utils = require("utils")
+    require("mini.surround").setup({
+      -- Add custom surroundings to be used on top of builtin ones. For more
+      -- information with examples, see `:h MiniSurround.config`.
+      custom_surroundings = nil,
 
-    -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
-    highlight_duration = 500,
+      -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+      highlight_duration = 500,
 
-    -- Module mappings. Use `''` (empty string) to disable one.
-    mappings = {
-      add = "ys", -- Add surrounding in Normal and Visual modes
-      delete = "ds", -- Delete surrounding
-      find = "Sf", -- Find surrounding (to the right)
-      find_left = "SF", -- Find surrounding (to the left)
-      highlight = "Sh", -- Highlight surrounding
-      replace = "cs", -- Replace surrounding
-      update_n_lines = "Sn", -- Update `n_lines`
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        add = "ys", -- Add surrounding in Normal and Visual modes
+        delete = "ds", -- Delete surrounding
+        find = "Sf", -- Find surrounding (to the right)
+        find_left = "SF", -- Find surrounding (to the left)
+        highlight = "Sh", -- Highlight surrounding
+        replace = "cs", -- Replace surrounding
+        update_n_lines = "Sn", -- Update `n_lines`
 
-      suffix_last = "l", -- Suffix to search with "prev" method
-      suffix_next = "n", -- Suffix to search with "next" method
-    },
+        suffix_last = "l", -- Suffix to search with "prev" method
+        suffix_next = "n", -- Suffix to search with "next" method
+      },
 
-    -- Number of lines within which surrounding is searched
-    n_lines = 20,
-  },
+      -- Number of lines within which surrounding is searched
+      n_lines = 20,
+    })
+
+    utils.map("n", "yS", "ys$")
+    utils.map("n", "yss", "^yS")
+  end,
 }
